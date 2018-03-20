89d30f62709c83d69f7232d10edc6744aad51f55
llafuente <llafuente@noboxout.com>
2018-03-20 14:02:21 +0100

    perf: bb-section-content now uses only two pipes

--- src/components/BBSectionContent.component.ts
+++ src/components/BBSectionContent.component.ts
@@ -1,34 +1,45 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Subject } from "rxjs/Subject";
 
+// <pre>
+// isLoading? {{subject | isLoading | json}}
+// isError? {{subject | isError | json}}
+// </pre>
 @Component({
   selector: "bb-section-content",
   template: `
-<div *ngIf="subject | isLoading">
+<div [ngSwitch]="subject | isLoading">
+  <div *ngSwitchCase="true">
 
-  <bb-progress></bb-progress>
+    <bb-progress></bb-progress>
 
-  <alert type="info" *ngIf="loadingMessage" class="text-center mt-2">
-    {{loadingMessage}}
-  </alert>
+    <alert type="info" *ngIf="loadingMessage" class="text-center mt-2">
+      {{loadingMessage}}
+    </alert>
 
-</div>
+  </div>
 
-<div *ngIf="!(subject | isError) && !(subject | isLoading)">
-  <ng-content></ng-content>
-</div>
+  <div *ngSwitchCase="false">
+    <div [ngSwitch]="subject | isError">
 
-<div *ngIf="(subject | isError) && !(subject | isLoading)" class="container text-center">
-  <alert type="danger" class="mt-2">
-    {{errorMessage || 'Error al obtener la informacion solicitada.'}}
-  </alert>
+      <div *ngSwitchCase="false">
+        <ng-content></ng-content>
+      </div>
 
-  <button
-    class="btn"
-    type="button"
-    (click)="doReload()">
-    <i class="fa fa-refresh"></i> Volver a intentarlo
-  </button>
+      <div *ngSwitchCase="true" class="container text-center">
+        <alert type="danger" class="mt-2">
+          {{errorMessage || 'Error al obtener la informacion solicitada.'}}
+        </alert>
+
+        <button
+          class="btn"
+          type="button"
+          (click)="doReload()">
+          <i class="fa fa-refresh"></i> Volver a intentarlo
+        </button>
+      </div>
+    </div>
+  </div>
 </div>
   `,
 })
@@ -40,6 +51,7 @@ export class BBSectionContentComponent {
   //@Input() hideFooterWhileLoading: boolean = true
 
   doReload() {
+    this.subject.error(null);
     this.subject = null;
     this.onReload.next(null);
   }
