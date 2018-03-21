69592aa2053435ca68bc83b6a6d9f708e44558a6
llafuente <llafuente@noboxout.com>
2018-03-21 12:12:47 +0100

    fix: error display condition

--- src/components/forms/BBErrors.component.ts
+++ src/components/forms/BBErrors.component.ts
@@ -4,24 +4,30 @@ import { BBErrorMessages } from "./BBErrorMessages.service";
 
 // <pre>valid? {{model.valid | json}} dirty? {{model.dirty | json}} touched? {{model.touched | json}}</pre>
 
+/**
+ * Display common errors errors
+ * To configure error messages use: BBErrorMessages
+ * Note angular set valid=false when control is disabled
+ *
+ */
 @Component({
   selector: "bb-errors",
   template: `
-<div class="alert alert-danger" *ngIf="!model.valid && (model.dirty || model.touched)">
-  <div *ngIf="model?.errors?.required" class="form-control-feedback">{{messages.required}}</div>
-  <div *ngIf="model?.errors?.minlength" class="form-control-feedback">{{messages.minlength}}</div>
-  <div *ngIf="model?.errors?.maxlength" class="form-control-feedback">{{messages.maxlength}}</div>
-  <div *ngIf="model?.errors?.email" class="form-control-feedback">{{messages.email}}</div>
-  <div *ngIf="model?.errors?.url" class="form-control-feedback">{{messages.url}}</div>
-  <div *ngIf="model?.errors?.pattern" class="form-control-feedback">{{pattern || messages.pattern}}</div>
-  <div *ngIf="model?.errors?.number" class="form-control-feedback">{{messages.number || messages.pattern}}</div>
+<div class="alert alert-danger" *ngIf="!model.valid && model?.errors && (model.dirty || model.touched)">
+  <div *ngIf="model.errors?.required" class="form-control-feedback">{{messages.required}}</div>
+  <div *ngIf="model.errors?.minlength" class="form-control-feedback">{{messages.minlength}}</div>
+  <div *ngIf="model.errors?.maxlength" class="form-control-feedback">{{messages.maxlength}}</div>
+  <div *ngIf="model.errors?.email" class="form-control-feedback">{{messages.email}}</div>
+  <div *ngIf="model.errors?.url" class="form-control-feedback">{{messages.url}}</div>
+  <div *ngIf="model.errors?.pattern" class="form-control-feedback">{{pattern || messages.pattern}}</div>
+  <div *ngIf="model.errors?.number" class="form-control-feedback">{{messages.number || messages.pattern}}</div>
 </div>
   `,
 })
 export class BBErrorsComponent {
-  // required
+  /** required */
   @Input() model: NgModel;
-  // override pattern message
+  /** override pattern message */
   @Input() pattern: string;
 
   constructor(public messages: BBErrorMessages) {}
