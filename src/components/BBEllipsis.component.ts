9de5608680f865f6b8d32cb00409a387372f573a
llafuente <llafuente@noboxout.com>
2018-04-10 14:47:28 +0200

    fix: visibility issues with hidden elippsis

--- src/components/BBEllipsis.component.ts
+++ src/components/BBEllipsis.component.ts
@@ -1,19 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, AfterViewChecked, ElementRef } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, AfterViewChecked, ElementRef, OnDestroy } from "@angular/core";
 import * as ftellipsis from "ftellipsis";
 
 @Component({
   selector: "bb-ellipsis",
   template: `
-<div>
-  <div [style.height]="height + 'px'">
+<div [style.maxHeight]="height + 'px'">
+  <div>
     <ng-content></ng-content>
   </div>
 </div>
   `,
 })
-export class BBEllipsisComponent implements OnInit, AfterViewChecked {
+export class BBEllipsisComponent implements OnInit, AfterViewChecked, OnDestroy {
   @Input() height: number;
   ellipsis: any;
+
+  interval: number = null;
 
   constructor(
     public element: ElementRef
@@ -23,8 +25,20 @@ export class BBEllipsisComponent implements OnInit, AfterViewChecked {
   ngOnInit() {
     const el = this.element.nativeElement as HTMLElement;
     const div = el.children.item(0);
+    const innerDiv = div.children.item(0) as HTMLElement;
 
     this.ellipsis = new ftellipsis.Ellipsis(div);
+
+    this.interval = setInterval(() => {
+      if (parseInt(innerDiv.style.height, 10) === 0) {
+        innerDiv.style.height = null;
+        this.ngAfterViewChecked();
+      }
+    }, 250);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
   ngAfterViewChecked() {
